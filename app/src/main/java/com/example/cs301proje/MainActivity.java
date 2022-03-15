0fb867952c6ae711a0a5b19af603d72da0588263
Claire <porterc24@up.edu>
2022-03-14 17:16:03 -0700

    MainActivity commit

--- app/src/main/java/com/example/cs301proje/MainActivity.java
+++ app/src/main/java/com/example/cs301proje/MainActivity.java
@@ -22,6 +22,6 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     @Override
     public void onClick(View view) {
         editText.setText("");
-        
+
        }
     }
